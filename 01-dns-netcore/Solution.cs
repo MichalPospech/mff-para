a10923c2b6717cb782885b02ab92bb44d2ac8e68
Michal Pospěch <michal.pospech@gmail.com>
2021-03-11 20:28:07 +0100

    Proof of concept

--- 01-dns-netcore/Solution.cs
+++ 01-dns-netcore/Solution.cs
@@ -1,39 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
-using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace dns_netcore
 {
-	class RecursiveResolver : IRecursiveResolver
-	{
-		private IDNSClient dnsClient;
+    class RecursiveResolver : IRecursiveResolver
+    {
+        private IDNSClient dnsClient;
 
-		public RecursiveResolver(IDNSClient client)
-		{
-			this.dnsClient = client;
-		}
+        private ConcurrentDictionary<string, IP4Addr> cache;
+        public RecursiveResolver(IDNSClient client)
+        {
+            this.dnsClient = client;
+            cache = new ConcurrentDictionary<string, IP4Addr>();
+        }
 
-		public Task<IP4Addr> ResolveRecursive(string domain)
-		{
-			/*
+        public Task<IP4Addr> ResolveRecursive(string domain)
+        {
+            /*
 			 * Just copy-pasted code from serial resolver.
 			 * Replace it with your implementation.
 			 * Also you may change this method to async (it will work with the interface).
 			 */
 
-			return Task<IP4Addr>.Run(() => {
-				string[] domains = domain.Split('.');
-				Array.Reverse(domains);
-				IP4Addr res = dnsClient.GetRootServers()[0];
-				foreach (var sub in domains) {
-					var t = dnsClient.Resolve(res, sub);
-					t.Wait();
-					res = t.Result;
-				}
-				return res;
-			});
-		}
-	}
+            return Task.Run(() =>
+            { 
+                var baseResolver = dnsClient.GetRootServers().First();
+
+                var subdomains = DomainToSubdomains(domain).Reverse();
+                var tasks = subdomains.Select(async sd =>
+                {
+
+                    var result = await CacheLookup(sd.Domain);
+                    return new
+                    {
+                        Addr = result,
+                        Domain = sd
+                    };
+                });
+
+                var results = Task.WhenAll(tasks).Result;
+                var incorrect = results.TakeWhile(r => r.Addr == null).Select(r => r.Domain).Reverse();
+                var resolver = results.Select(r => r.Addr).FirstOrDefault(addr => addr != null) ?? baseResolver; 
+
+                foreach (var sub in incorrect)
+                {
+                    var t = dnsClient.Resolve(resolver, sub.LowestLevel);
+                    resolver = t.Result;
+					cache.TryAdd(sub.Domain, resolver);
+                }
+                return resolver;
+            });
+        }
+
+        private Task<IP4Addr?> CacheLookup(string domain)
+        {
+            return Task.Run(() =>
+            {
+                var present = cache.TryGetValue(domain, out var addr);
+                if (present)
+                {
+                    var reverse = dnsClient.Reverse(addr).Result;
+                    if (reverse != domain)
+                    {
+                        cache.TryRemove(domain, out var _);
+                    }
+                    return reverse == domain ? addr : null;
+                }
+                return (IP4Addr?)null;
+            });
+        }
+
+        private IEnumerable<Subdomain> DomainToSubdomains(string domain)
+        {
+            var split = domain.Split('.');
+            Subdomain sub = null;
+            foreach (var subdomain in split.Reverse())
+            {
+                if (sub != null)
+                {
+                    sub = new Subdomain
+                    {
+                        LowestLevel = subdomain,
+                        Domain = $"{subdomain}.{sub.Domain}"
+                    };
+                    yield return sub;
+                    continue;
+                }
+                sub = new Subdomain
+                {
+                    Domain = subdomain,
+                    LowestLevel = subdomain
+                };
+                yield return sub;
+            }
+        }
+
+        private class Subdomain
+        {
+            public string LowestLevel { get; set; }
+            public string Domain { get; set; }
+        }
+    }
 }
