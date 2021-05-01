ba026d335d70721467f8cc11516774b72f80bfcb
Michal Pospěch <michal.pospech@gmail.com>
2021-05-01 03:05:40 +0200

    bugfix

--- 03-levenshtein/framework/implementation.hpp
+++ 03-levenshtein/framework/implementation.hpp
@@ -7,7 +7,6 @@
 
 #include <utility>
 #include <algorithm>
-#include <iostream>
 
 #define BLOCK_SIZE 1024
 
@@ -25,22 +24,19 @@ private:
 	std::size_t max_blocks;
 
 private:
-	void calculate_submatrix(const std::vector<C> &str1, const std::vector<C> &str2, std::size_t x1, std::size_t x2, std::size_t y1, std::size_t y2, std::size_t start_index)
+	void calculate_submatrix(const C *str1, const C *str2, std::size_t x1, std::size_t x2, std::size_t y1, std::size_t y2, std::size_t start_index)
 	{
-		// std::cout << x1 << " " << x2 << " " << y1 << " " << y2 << " " << start_index << " " << end_index <<  std::endl;
+
 		std::size_t x_size = x2 - x1;
 		std::size_t y_size = y2 - y1;
 		for (size_t x = 0; x < x_size; x++)
 		{
-			#pragma omp simd
+#pragma omp simd
 			for (size_t y = 0; y < y_size; y++)
 			{
-				// std::cout << x << " " << y << std::endl;
 				std::size_t sub_cost = (str1[x1 + x] == str2[y1 + y]) ? 0 : 1;
 				std::size_t diag_index = start_index + y_size - y + x;
 				distances[diag_index] = std::min({distances[diag_index] + sub_cost, distances[diag_index - 1] + 1, distances[diag_index + 1] + 1});
-				// std::cout << x1 + x << " " << y1 + y << std::endl;
-				// std::cout << distances[diag_index] << std::endl;
 			}
 		}
 	}
@@ -52,10 +48,14 @@ public:
 	 */
 	virtual void init(DIST len1, DIST len2)
 	{
+		if (len1 < len2)
+		{
+			std::swap(len1, len2);
+		}
 		this->len1 = len1;
 		this->len2 = len2;
-		num_blocks1 = len1 / BLOCK_SIZE + ((len1 % BLOCK_SIZE) ? 1 : 0);
-		num_blocks2 = len2 / BLOCK_SIZE + ((len2 % BLOCK_SIZE) ? 1 : 0);
+		num_blocks1 = len1 / BLOCK_SIZE;
+		num_blocks2 = len2 / BLOCK_SIZE;
 		num_diags = num_blocks1 + num_blocks2 - 1;
 		distances.resize(len1 + len2 + 1);
 		for (size_t i = len2 + 1; i < len2 + len1 + 2; i++)
@@ -78,34 +78,26 @@ public:
 
 	virtual DIST compute(const std::vector<C> &str1, const std::vector<C> &str2)
 	{
-		//TODO different sizes (ensure one is smaller?)
+		const C *s1 = &str1[0];
+		const C *s2 = &str2[0];
+		if (str1.size() < str2.size())
+			std::swap(s1, s2);
 		for (std::size_t i = 0; i < num_diags; i++)
 		{
-			// std::cout << "==========================" << std::endl;
 			std::size_t diag_num_blocks = (i < min_blocks) ? (i + 1) : ((i < max_blocks) ? min_blocks : (num_diags - i));
-			// std::cout << "diag " << i << " num blocks " << diag_num_blocks << std::endl;
-			#pragma omp parallel for
+#pragma omp parallel for schedule(static)
 			for (std::size_t j = 0; j < diag_num_blocks; j++)
 			{
-				// std::cout << "------------------" << std::endl;
 
 				std::size_t x_index = (i < min_blocks) ? j : (i - min_blocks + 1 + j);
 				std::size_t y_index = (i < min_blocks) ? (i - j) : (min_blocks - 1 - j);
 				std::size_t x1 = x_index * BLOCK_SIZE;
 				std::size_t y1 = y_index * BLOCK_SIZE;
-				std::size_t x2 = std::min(len1, x1 + BLOCK_SIZE);
-				std::size_t y2 = std::min(len2, y1 + BLOCK_SIZE);
+				std::size_t x2 = x1 + BLOCK_SIZE;
+				std::size_t y2 = y1 + BLOCK_SIZE;
 				std::size_t start_index = len2 - y2 + x1;
-
-				// std::cout << x_index << " " << y_index << std::endl;
-				// std::cout << x1 << " " << x2 << " " << y1 << " " << y2 << " " << start_index << std::endl;
-
-				calculate_submatrix(str1, str2, x1, x2, y1, y2, start_index);
+				calculate_submatrix(s1, s2, x1, x2, y1, y2, start_index);
 			}
-			// for (auto &&i : distances)
-			// {
-			// 	std::cout << i << std::endl;
-			// }
 		}
 
 		return distances[len1];
